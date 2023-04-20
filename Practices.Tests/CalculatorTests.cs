83a12056cc8b4d76053493b1ef5392e18b9eeca3
6eh <mcls@list.ru>
2023-04-20 22:38:37 +0200

    SkillFactory Module 16 Final Task
    SkillFactory Module 16 Final Task

---
+++ Practices.Tests/CalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Practices.Tests;
+
+public class CalculatorTests
+{
+    [Test]
+    public void AdditionalMustReturnCorrectValue()
+    {
+        Calculator calculator = new ();
+        Assert.That(calculator.Additional(1, 1) == 2);
+    }
+
+    [Test]
+    public void SubtractionMustReturnCorrectValue()
+    {
+        Calculator calculator = new();
+        Assert.True(calculator.Subtraction(8, 1) == 7);
+    }
+
+    [Test]
+    public void MiltiplicationMustReturnCorrectValue()
+    {
+        Calculator calculator = new();
+        Assert.True(calculator.Miltiplication(8, 10) == 80);
+    }
+
+    [Test]
+    public void DivisionMustReturnCorrectValue()
+    {
+        Calculator calculator = new();
+        Assert.That(calculator.Division(100, 2) == 50);
+    }
+}
---
+++ Practices/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace Practices;
+
+/* SkillFactory Module 16 Final Task
+ * Протестируйте все методы класса Calculator с использованием NUnit-фреймворка.
+ */
+
+public class Calculator
+{
+    public int Additional(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtraction(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Miltiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Division(int a, int b)
+    {
+        return a / b;
+    }
+}
+
